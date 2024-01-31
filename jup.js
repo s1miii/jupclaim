dac10bc50748910bc19c0556c1d54c5dedaaac0e
Simi <84782994+s1miii@users.noreply.github.com>
2024-01-31 17:07:09 +0700

    Create jup.js

---
+++ jup.js
@@ -0,0 +1,63 @@
+import { getOrCreateAssociatedTokenAccount, createTransferInstruction } from "@solana/spl-token";
+import { Connection, Keypair, ParsedAccountData, PublicKey, sendAndConfirmTransaction, Transaction } from "@solana/web3.js";
+import { Keypair } from "@solana/web3.js";
+const secret = [6,78,123,179,112,90,228,244,163,89,107,160,228,74,197,53,96,252,160,231,224,142,247,139,156,127,143,144,12,15,81,123,94,47,190,63,101,120,136,184,156,187,96,80,97,133,187,8,52,187,119,27,98,30,30,39,215,35,47,148,70,169,139,60]; // 👈 Replace with your secret
+const FROM_KEYPAIR = Keypair.fromSecretKey(new Uint8Array(secret));
+console.log(`My public key is: ${FROM_KEYPAIR.publicKey.toString()}.`);
+const QUICKNODE_RPC = 'https://example.solana-devnet.quiknode.pro/0123456/';
+const SOLANA_CONNECTION = new Connection(QUICKNODE_RPC);
+const DESTINATION_WALLET = 'RECIVED ADDRESS'; 
+const MINT_ADDRESS = 'JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN'; //You must change this value!
+const TRANSFER_AMOUNT = 200;
+async function getNumberDecimals(mintAddress: string):Promise<number> {
+    const info = await SOLANA_CONNECTION.getParsedAccountInfo(new PublicKey(MINT_ADDRESS));
+    const result = (info.value?.data as ParsedAccountData).parsed.info.decimals as number;
+    return result;
+}
+async function sendTokens() {
+}
+    console.log(`Sending ${TRANSFER_AMOUNT} ${(MINT_ADDRESS)} from ${(FROM_KEYPAIR.publicKey.toString())} to ${(DESTINATION_WALLET)}.`)
+    //Step 1
+    console.log(`1 - Getting Source Token Account`);
+    let sourceAccount = await getOrCreateAssociatedTokenAccount(
+        SOLANA_CONNECTION, 
+        FROM_KEYPAIR,
+        new PublicKey(MINT_ADDRESS),
+        FROM_KEYPAIR.publicKey
+    );
+    console.log(`    Source Account: ${sourceAccount.address.toString()}`);
+
+    //Step 2
+    console.log(`2 - Getting Destination Token Account`);
+    let destinationAccount = await getOrCreateAssociatedTokenAccount(
+        SOLANA_CONNECTION, 
+        FROM_KEYPAIR,
+        new PublicKey(MINT_ADDRESS),
+        new PublicKey(DESTINATION_WALLET)
+    );
+    console.log(`    Destination Account: ${destinationAccount.address.toString()}`);
+
+    //Step 3
+    console.log(`3 - Fetching Number of Decimals for Mint: ${MINT_ADDRESS}`);
+    const numberDecimals = await getNumberDecimals(MINT_ADDRESS);
+    console.log(`    Number of Decimals: ${numberDecimals}`);
+
+    //Step 4
+    console.log(`4 - Creating and Sending Transaction`);
+    const tx = new Transaction();
+    tx.add(createTransferInstruction(
+        sourceAccount.address,
+        destinationAccount.address,
+        FROM_KEYPAIR.publicKey,
+        TRANSFER_AMOUNT * Math.pow(10, numberDecimals)
+    ))
+
+    const latestBlockHash = await SOLANA_CONNECTION.getLatestBlockhash('confirmed');
+    tx.recentBlockhash = await latestBlockHash.blockhash;    
+    const signature = await sendAndConfirmTransaction(SOLANA_CONNECTION,tx,[FROM_KEYPAIR]);
+    console.log(
+        '\x1b[32m', //Green Text
+        `   Transaction Success!🎉`,
+        `\n    https://explorer.solana.com/tx/${signature}?cluster=devnet`
+    );
+sendTokens();
